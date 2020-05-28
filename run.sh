184d7ae48e4cf889db272b4eb537940d7c4340f7
moelife-coder <61054382+moelife-coder@users.noreply.github.com>
2020-05-28 17:58:34 +0800

    Update network name

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ train_mlp_mnist() {
   python Train.py --epoch 20 --dataset mnist --network mlp --loss_func "$2" --model_name "$3" 784 "$1"
 }
 train_cnn_mnist() {
-  python Train.py --epoch 20 --dataset mnist --network cnn --loss_func "$2" --model_name "$3" 1 "$1"
+  python Train.py --epoch 20 --dataset mnist --network conv --loss_func "$2" --model_name "$3" 1 "$1"
 }
 train_mlp_cifar() {
   python Train.py --epoch 20 --dataset cifar10 --network mlp --loss_func "$2" --model_name "$3" 3072 "$1"
 }
 train_cnn_cifar() {
-  python Train.py --epoch 20 --dataset cifar10 --network cnn --loss_func "$2" --model_name "$3" 3 "$1"
+  python Train.py --epoch 20 --dataset cifar10 --network conv --loss_func "$2" --model_name "$3" 3 "$1"
 }
