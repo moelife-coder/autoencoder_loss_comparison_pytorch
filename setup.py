936dec33aa5d34dc00c4e92f3dd02f35259a6e5a
moelife-coder <61054382+moelife-coder@users.noreply.github.com>
2020-05-28 17:05:54 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'autoencoder_loss_comparison_pytorch',
+    version = '1.0',
+    packages = ['pytorch_ssim'],
+    url = 'https://github.com/moelife-coder/autoencoder_loss_comparison_pytorch',
+    license = '',
+    author = 'Tony Zou',
+    author_email = 'zoustone@hotmail.com',
+    description = ''
+)
